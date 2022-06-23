12b5b3d0383a57b4d7e8d43eb0c129d2f425c381
Rostrv <trovatorosario91@gmail.com>
2022-06-23 17:30:27 +0200

    added one to many relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'content', 'slug', 'cover_image'];
+    
+
+    public function category(): BelongsTo
+{
+    return $this->belongsTo(Category::class);
 }
+}
+
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Programming', 'Automation', 'Web design', 'Best Practices'];
+        foreach ($categories as $category) {
+            $cat = new Category();
+            $cat->name = $category;
+            $cat->slug = Str::slug($category);
+            $cat->save();
+        }
+    }
+}
